c388e9d1894f6e3cc63062163b4d1ba632daa153
Alok Dev <mk.alok806@gmail.com>
2026-05-03 17:41:27 +0530

    Add files via upload

---
+++ Java-Learning-Journey/01-Introduction/First_Program.java
@@ -0,0 +1,16 @@
+
+public class First_Program {
+
+    public static void main(String[] args) {
+        // This is how we print our first line of code:
+        System.out.println("This is my First Java Program");
+
+        // This is a single line comment.
+
+        /*
+    This is 
+    a multi-line
+    comment.
+         */
+    }
+}

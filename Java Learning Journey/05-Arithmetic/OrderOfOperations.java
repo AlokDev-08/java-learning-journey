7ad3fdaa966a4ef0e554eb6e7025ee94d1b942ba
Alok Dev <mk.alok806@gmail.com>
2026-05-07 13:16:50 +0530

    Add files via upload

---
+++ Java Learning Journey/05-Arithmetic/OrderOfOperations.java
@@ -0,0 +1,18 @@
+public class OrderOfOperations {
+    public static void main(String[] args) {
+
+//        Order of Operations [ P-E-M-D-A-S]
+//        P - Parenthesis
+//        E - Exponents
+//        M - Multiplication
+//        D - Division
+//        A - Addition
+//        S - Substraction
+
+
+        double result = 3+4*(7-5)/2.0;
+
+        System.out.println(result);
+
+    }
+}

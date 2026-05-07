e7dcc8bf3ec3a640c49e6b31b1ffc61fdc6785c8
Alok Dev <mk.alok806@gmail.com>
2026-05-07 13:16:37 +0530

    Add files via upload

---
+++ Java Learning Journey/05-Arithmetic/IncrementAndDecrementOperators.java
@@ -0,0 +1,23 @@
+public class IncrementAndDecrementOperators {
+    public static void main(String[] args) {
+
+//        Increment and Decrement Operators
+
+        int x  = 1;
+
+        x++;
+        x++;
+        x++;
+
+        System.out.println(x);
+
+        int y = 2;
+
+        y--;
+        y--;
+        y--;
+
+        System.out.println(y);
+
+    }
+}

7328fbdc1b88548bf544781b965d3e8697d93f6b
Alok Dev <mk.alok806@gmail.com>
2026-05-13 22:08:28 +0530

    Add files via upload

---
+++ Java Learning Journey/27-Methods/Cube.java
@@ -0,0 +1,9 @@
+public class Cube {
+    public static void main(String[]args){
+        double number = cube(9);
+        System.out.println(number);
+    }
+    static double cube(double number){
+        return number*number*number;
+    }
+}
